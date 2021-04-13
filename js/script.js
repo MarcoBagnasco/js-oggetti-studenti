7221957f2c53721d4bc4d4d1baec73d188974819
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-13 16:02:07 +0200

    add task 1 html

--- js/script.js
+++ js/script.js
@@ -7,12 +7,21 @@ $(document).ready(function () {
      * 3.Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. Una volta creato il nuovo oggetto inserirlo nell'array creato al punto 2 e mostrare alla fine il contenuto dell'array. 
      *********/
 
+    // REFERENCE
+    var obj = $('#template .object').clone();
+    console.log(obj);
     //1.
     var student = {
         nome: 'Paolo',
         cognome: 'Duzioni',
         eta: 39
     };
+
+    obj.find('.first-name').append(student.nome);
+    obj.find('.last-name').append(student.cognome);
+    obj.find('.age').append(student.eta);
+
+    $('.task-1').append(obj);
     
     for(var key in student){
         console.log(key + ': ' + student[key]);
@@ -44,19 +53,19 @@ $(document).ready(function () {
     }
 
     //3.
-    var newStudent = {
-        nome: prompt('Enter your first name').trim(),
-        cognome: prompt('Enter your last name').trim(),
-        eta: parseInt(prompt('Enter your age').trim())
-    }
+    // var newStudent = {
+    //     nome: prompt('Enter your first name').trim(),
+    //     cognome: prompt('Enter your last name').trim(),
+    //     eta: parseInt(prompt('Enter your age').trim())
+    // }
 
-    students.push(newStudent);
+    // students.push(newStudent);
     
-    for(var i = 0; i < students.length; i++){
-        console.log('Student ' + (i + 1) + '\n\t' + 
-        'Nome: ' + students[i].nome + '\n\t' + 
-        'Cognome: ' + students[i].cognome + '\n\t' +
-        'Età: ' + students[i].eta);
-    }
+    // for(var i = 0; i < students.length; i++){
+    //     console.log('Student ' + (i + 1) + '\n\t' + 
+    //     'Nome: ' + students[i].nome + '\n\t' + 
+    //     'Cognome: ' + students[i].cognome + '\n\t' +
+    //     'Età: ' + students[i].eta);
+    // }
     //End Doc Ready
 });
