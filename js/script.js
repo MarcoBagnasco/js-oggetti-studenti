49de179681e0fac29b448c08cc8babce6b2867b1
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-13 15:11:53 +0200

    add task 1

--- js/script.js
+++ js/script.js
@@ -7,7 +7,16 @@ $(document).ready(function () {
      * 3.Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. Una volta creato il nuovo oggetto inserirlo nell'array creato al punto 2 e mostrare alla fine il contenuto dell'array. 
      *********/
 
-    console.log($);//test
+    //1.
+    var student = {
+        nome: 'Paolo',
+        cognome: 'Duzioni',
+        eta: 39
+    }
     
+    for(var key in student){
+        console.log(student[key]);
+    }
+
     //End Doc Ready
 });
